d93eecd1a13c3e2314070dfcb2d9319ee2fe82d1
Daniel Aleixander <idirribarrren@gmail.com>
2020-11-23 10:06:56 -0500

    subiendo primera pregunta

---
+++ primera.php
@@ -0,0 +1,14 @@
+<?php  
+
+$array = array( 
+    4 => 'blanco',  
+    6 => 'verde',  
+    11 => 'roja'
+); 
+  
+foreach($array as $name) { 
+
+    echo "el rsultado esperado es: $name "; 
+    break;  
+} 
+?> 
